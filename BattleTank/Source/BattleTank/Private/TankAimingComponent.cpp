0b637d09a9ba9031c9f3fab3ca074bb09df90ae1
Steven Lee <ryukishigami@gmail.com>
2017-04-17 17:18:15 +0800

    Lecture 142 Fire skeleton and projectile model

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -40,11 +40,11 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed) {
 		//UE_LOG(LogTemp, Warning, TEXT("Aiming at %s"), *AimDirection.ToString());
 		MoveBarrelTowards(AimDirection);
 		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f, Aim solution found"), Time);
+		//UE_LOG(LogTemp, Warning, TEXT("%f, Aim solution found"), Time);
 	}
 	else {
 		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f, No aim solution found"), Time);
+		//UE_LOG(LogTemp, Warning, TEXT("%f, No aim solution found"), Time);
 	}
 
 
