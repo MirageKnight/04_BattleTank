79d4386e53c664b453d28d8e6c3e9d8195c4cbca
Steven Lee <ryukishigami@gmail.com>
2017-04-22 22:26:20 +0800

    Lecture 153 Starting Fly by wire

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -8,7 +8,8 @@ void UTankTrack::SetThrtottle(float Throttle) {
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle = %f"), *Name, Throttle);
 
-	auto ForceApplied = GetForwardVector() * Throttle *TrackMaxDrivingForce;
+	// ForwardVector is the direction, throttle is percent of max force, and multiplied by max force
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
